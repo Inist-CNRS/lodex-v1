1af3c6d6324285003f0c2278cd22c71447b22d82
Nicolas Thouvenin <nthouvenin@gmail.com>
2016-05-15 22:26:45 +0200

    add tabs

--- components/TablesItem.vue
+++ components/TablesItem.vue
@@ -2,8 +2,7 @@
 <div id="tables-item" class="row">
 	<div class="pull-right">
 		<div class="btn-group" role="group" aria-label="...">
-			<button type="button" class="btn btn-default"> <icon name="pencil-square-o" label="Edit"> </button>
-			<button type="button" class="btn btn-default"> <icon name="code-fork" label="Fork"> </button>
+			<button type="button" class="btn btn-default" @click="showModal = true"> <icon name="code-fork" label="Fork"> </button>
 			<button type="button" class="btn btn-default"> <icon name="upload" label="Upload"> </button>
 			<button type="button" class="btn btn-default"> <icon name="download" label="Download">  </button>
 		</div>
@@ -12,9 +11,18 @@
 		<span>Current: {{ store.currentTable.value }} </span>
 	</div>
 </div>
+
+<modal :show.sync="showModal">
+  <div slot="modal-header" class="modal-header">
+    <h4 class="modal-title">Modal title</h4>
+  </div>
+  <div slot="modal-body" class="modal-body">...</div>
+</modal>
+
 </template>
 <script>
 import sharedStore from './store.js'
+
 
 export default {
 	data () {
--- components/backoffice.js
+++ components/backoffice.js
@@ -3,8 +3,9 @@ var Vue = require('vue');
 
 
 var Icon = require('vue-awesome/dist/vue-awesome');
-
 Vue.component('icon', Icon);
+Vue.component('modal', require('vue-strap/dist/vue-strap.min').modal)
+
 
 Vue.use(require('vue-resource'));
 Vue.use(require('vue-infinite-scroll'));
