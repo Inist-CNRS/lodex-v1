08ffce763140f1a87d56d49756cde661b720b14d
Nicolas Thouvenin <nthouvenin@gmail.com>
2016-05-17 22:15:16 +0200

    3 modeals

--- components/TablesItem.vue
+++ components/TablesItem.vue
@@ -16,18 +16,35 @@
 </div>
 
 <modal :show.sync="showModalUpload" title="Upload to this table">
-  <div slot="modal-body" class="modal-body">B</div>
+  <div slot="modal-body" class="modal-body">
+
+  <inputfile label="Browse" help="Try selecting one or more files and watch the feedback"><inputfile>
+  
+  </div>
+  <div slot="modal-footer" class="modal-footer">
+    <button type="button" class="btn btn-default" @click='showModalUpload = false'>Close</button>
+    <button type="button" class="btn btn-success" @click='doUpload()'>Custom Save</button>
+  </div>
 </modal>
 <modal :show.sync="showModalDownload" title="Download this table">
   <div slot="modal-body" class="modal-body">C</div>
+  <div slot="modal-footer" class="modal-footer">
+    <button type="button" class="btn btn-default" @click='showModalDownload = false'>Close</button>
+    <button type="button" class="btn btn-success" @click='doDownload()'>Custom Save</button>
+  </div>
 </modal>
 <modal :show.sync="showModalFork" title="Fork this table">
 	<div slot="modal-body" class="modal-body">A<div>
+	<div slot="modal-footer" class="modal-footer">
+		<button type="button" class="btn btn-default" @click='showModalFork = false'>Close</button>
+		<button type="button" class="btn btn-success" @click='doFork()'>Custom Save</button>
+	</div>
 </modal>
 
 </template>
 <script>
 import sharedStore from './store.js'
+import inputfile from './InputFile.vue'
 
 
 export default {
@@ -41,12 +58,15 @@ export default {
 	},
 	methods: {
 		doFork() {
+			this.showModalFork = false
 			alert('Forked');
 		},
 		doUpload() {
+			this.showModalUpload = false
 			alert('Uploaded');
 		},
 		doDownload() {
+			this.showModalDownload = false
 			alert('Download');
 		}
 	},
