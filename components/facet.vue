79c07e336f5e911c8a3df3ec1454779fc89695fd
Nicolas Thouvenin <nthouvenin@gmail.com>
2016-09-21 13:48:01 +0200

    introduce component

---
+++ components/facet.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import MQS from 'mongodb-querystring'
+export default {
+	data () {
+		return {
+			msg: 'Hello World!'
+		}
+	},
+	methods: {
+		reload(done) {
+			let self = this;
+			let qry = {
+				"field" : "_columns.title.content"
+			}
+			let url = window.location.protocol
+			.concat('//')
+			.concat(window.location.host)
+			.concat('/$distinct')
+			.concat(MQS.stringify(qry, {}));
+			self.$http.get(url).then(function (response) {
+				let rootTable = response.data[0]._id
+				let url = self.store.s
+			})
+}
+</script>
---
+++ components/list.js
@@ -0,0 +1,27 @@
+
+// The entryPoint calls the project components.
+
+/* global Vue */
+
+'use strict';
+var Vue = require('vue')
+
+Vue.use(require('vue-resource'));
+
+/*
+var facet = require('./facet.vue')
+
+new Vue({
+
+  el: '#facet',
+
+  components: {
+
+    // The left key corresponds to the html tag where the component is included.
+    // For exemple, we put the table.vue component
+    // into <instancestable></instancestable> in template.html.
+    facet: facet
+  }
+
+});
+*/
