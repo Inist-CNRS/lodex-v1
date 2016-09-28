394beefbc295edeb7dd7c91fea05de2a32636af3
François Parmentier <francois.parmentier@gmail.com>
2016-09-28 10:01:35 +0200

    :sparkles: Display the _wid in table / URI

--- components/tableau.vue
+++ components/tableau.vue
@@ -25,7 +25,7 @@
 		<tbody>
 		  <tr v-for="(index, doc) in items">
 				<td data-title="URI">
-					<a href="{{ doc._uri }}?alt=html">#</a>
+					<a href="{{ doc._uri }}?alt=html">{{ doc._wid }}</a>
 				</td>
 				<td v-for="(colkey, col) in columns">
 					{{{ doc._columns[colkey].content.raw }}}
