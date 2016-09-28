93793096eac092f00b107c64a814e0e26d890851
François Parmentier <francois.parmentier@gmail.com>
2016-09-28 09:36:57 +0200

    :shirt: Linting

--- downloaders/format.js
+++ downloaders/format.js
@@ -1,14 +1,12 @@
 'use strict';
 
-var path = require('path')
-  , basename = path.basename(__filename, '.js')
-  , debug = require('debug')('castor:downloaders:' + basename)
-  , htmlspecialchars = require('htmlspecialchars')
-  , XMLMapping = require('xml-mapping')
-  , moment = require('moment')
-  , marked = require('marked')
-  ;
-
+var path = require('path');
+var basename = path.basename(__filename, '.js');
+var debug = require('debug')('castor:downloaders:' + basename);
+var htmlspecialchars = require('htmlspecialchars');
+var XMLMapping = require('xml-mapping');
+var moment = require('moment');
+var marked = require('marked');
 
 
 module.exports = function(options, core) {
@@ -20,21 +18,23 @@ module.exports = function(options, core) {
       data._columns[col].content = {
         raw : data._columns[col].content,
         html: ''
-      }
+      };
       if (format === 'raw') {
-        data._columns[col].content.html = htmlspecialchars(data._columns[col].content.raw)
+        data._columns[col].content.html = htmlspecialchars(data._columns[col].content.raw);
       }
       else if (format === 'component') {
         data._columns[col].content.html = XMLMapping.dump(data._columns[col].content.raw);
       }
       else if (data._columns[col].type === 'date' && format) {
-        debug('date', typeof data._columns[col].content.raw, data._columns[col].content.raw, data._columns[col].format)
-        data._columns[col].content.html = moment(data._columns[col].content.raw).format(data._columns[col].format);
+        debug('date', typeof data._columns[col].content.raw, data._columns[col].content.raw,
+              data._columns[col].format);
+        data._columns[col].content.html = moment(data._columns[col].content.raw)
+                                         .format(data._columns[col].format);
       }
       else if (format === 'markdown') {
         data._columns[col].content.html = marked(data._columns[col].content.raw);
       }
-    })
+    });
     submit(null, data);
   };
 };
