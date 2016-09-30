833d08f7465e6a17e583a4543f88cfcf2e2543f4
Nicolas Thouvenin <nthouvenin@gmail.com>
2016-09-30 11:59:25 +0200

    all texts in the config (bis)

--- downloaders/render.js
+++ downloaders/render.js
@@ -1,12 +1,20 @@
 'use strict';
 
-var path = require('path'),
-  basename = path.basename(__filename, '.js'),
-  debug = require('debug')('castor:downloaders:' + basename)
+var path = require('path')
+  , basename = path.basename(__filename, '.js')
+  , debug = require('debug')('castor:downloaders:' + basename)
+  , marked = require('marked')
+  , traverse = require('traverse')
   ;
 
 module.exports = function(options, core) {
   options = options || {};
+  traverse(core.config.get('print')).forEach(function (x) {
+    if (x && typeof x === 'string') {
+      this.update(marked(x));
+    }
+  });
+
   return {
     breakPipe : true,
     onEach: function (data, submit) {
@@ -24,6 +32,7 @@ module.exports = function(options, core) {
       }
       var template = self.documents.length === 1 ? 'item.html' : 'list.html';
       var locals = self.documents.length === 1 ? self.documents[0] : { documents : self.documents };
+      locals.print = core.config.get('print');
       self.stream.render(template, locals);
     }
   };
