b74a912570e3e84fae55c4521648102caf451ef9
François Parmentier <francois.parmentier@gmail.com>
2016-10-12 18:24:11 +0200

    :sparkles: Add getByScheme nunjucks filter

--- app/engines/nunjucks.js
+++ app/engines/nunjucks.js
@@ -6,6 +6,23 @@ var path = require('path')
   , nunjucks = require('nunjucks')
   ;
 
+var getByScheme = function(document, scheme, cover, format) {
+  //debug('getByScheme(', document, ',', scheme, ',', cover);
+  cover = cover || 'collection';
+  format = format || 'html';
+  var fieldNames = Object.keys(document);
+
+  var wantedField = fieldNames
+  .map(function (fieldName) { return document[fieldName]; })
+  .filter(function (field) { return field.scheme === scheme; })
+  .filter(function (field) { return field.cover === cover; });
+
+  if (wantedField.length === 0) {
+    return '';
+  }
+  return wantedField[0].content[format];
+};
+
 module.exports = function(options, core) {
   options = options || {};
   options.autoescape = options.autoescape || false;
@@ -13,6 +30,7 @@ module.exports = function(options, core) {
     new nunjucks.FileSystemLoader(options.views || 'views'),
     options
   );
+  env.addFilter('getByScheme', getByScheme);
   return function (filePath, fileInput, callback) {
     env.render(filePath, fileInput, callback);
   };
