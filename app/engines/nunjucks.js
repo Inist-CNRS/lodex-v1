93db107ca28681bb81b4618aafa03847d6681833
François Parmentier <francois.parmentier@gmail.com>
2016-10-13 12:00:09 +0200

    :sparkles: Move optional parameters of getByScheme in options

--- app/engines/nunjucks.js
+++ app/engines/nunjucks.js
@@ -6,10 +6,62 @@ var path = require('path')
   , nunjucks = require('nunjucks')
   ;
 
-var getByScheme = function(document, scheme, cover, format) {
-  //debug('getByScheme(', document, ',', scheme, ',', cover);
-  cover = cover || 'collection';
-  format = format || 'html';
+/**
+ * Filter to get a field value in the document which uses a scheme
+ *
+ * Example of document:
+ * { title1:
+ *   { label: 'Dataset title',
+ *     scheme: 'http://purl.org/dc/terms/title',
+ *     type: 'string',
+ *     format: 'raw',
+ *     lang: 'en',
+ *     content: { raw: 'Films', html: 'Films' },
+ *     cover: 'dataset' },
+ *  desc1:
+ *   { label: 'Dataset description',
+ *     scheme: 'http://purl.org/dc/terms/description',
+ *     type: 'string',
+ *     format: 'markdown',
+ *     lang: 'en',
+ *     content:
+ *      { raw: 'Several films to demontrate the usage of __LODEX__',
+ *        html: '<p>Several films to demontrate the usage of <strong>LODEX</strong></p>\n' },
+ *     cover: 'dataset' },
+ *  title:
+ *   { title: 'Title',
+ *     stylesheet: { content: 'Rocky' },
+ *     label: 'Title',
+ *     scheme: 'http://purl.org/dc/terms/title',
+ *     type: 'string',
+ *     lang: 'fr',
+ *     displayAreas: { facet: false, table: true },
+ *     content: { raw: 'Rocky', html: 'Rocky' },
+ *     cover: 'collection' },
+ *  description:
+ *   { label: 'Description',
+ *     scheme: 'http://purl.org/dc/terms/description',
+ *     type: 'string',
+ *     content: { raw: 'John G. Avildsen', html: 'John G. Avildsen' },
+ *     cover: 'collection' }}
+ *
+ * Usage in HTML:
+ * <h2>{{ _columns|getByScheme('http://purl.org/dc/terms/title')|d('Missing "title"')}}</h2>
+ *
+ * @param  {Object} document contains the list of document's fields (with all attributes)
+ * @param  {string} scheme   URI of the property to find in the document
+ * @param  {Object} options  Any or all of the following options:
+ *                           - cover: 'dataset', 'collection' (default)
+ *                           - format: 'html' (default), 'raw'
+ *                           - language: 'fr', 'en' (not used by default)
+ * @return {string}          value of the document's fields matching the scheme (and the options)
+ */
+var getByScheme = function(document, scheme, options) {
+  debug('getByScheme', document);
+  options = options || {};
+  var cover = options.cover || 'collection';
+  var format = options.format || 'html';
+  var language = options.language;
   var fieldNames = Object.keys(document);
 
   var wantedField = fieldNames
@@ -19,6 +71,10 @@ var getByScheme = function(document, scheme, cover, format) {
 
   if (wantedField.length === 0) {
     return '';
+  }
+  if (language && wantedField.length > 1) {
+    wantedField = wantedField
+    .filter(function (field) { return field.lang === language; });
   }
   return wantedField[0].content[format];
 };
