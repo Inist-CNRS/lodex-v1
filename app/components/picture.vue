c706d5f391b311f7ddb0f1086134b3a8bb38b8a5
François Parmentier <francois.parmentier@gmail.com>
2016-11-02 18:32:27 +0100

    :sparkles: Add zoom in picture component

--- app/components/picture.vue
+++ app/components/picture.vue
@@ -1,14 +1,48 @@
 <template>
 	<div>
-		<img v-bind:height="height" class="known-uri-icon" v-bind:src="src">
+		<img v-bind:height="realHeight" class="known-uri-icon" v-bind:class="{ 'zoom-in': zoomIn, 'zoom-out': zoomOut }" v-bind:src="src" @click="toggleZoom">
 	</div>
 </template>
 
 <script>
 export default {
-  props: ['src', 'height']
+  props: ['src', 'height'],
+	data () {
+    return {
+			zoomIn: false,
+			zoomOut: true,
+			realHeight: this.height
+    }
+  },
+	methods: {
+		toggleZoom () {
+			console.log("Oui!")
+			this.zoomIn = !this.zoomIn
+			this.zoomOut = !this.zoomOut
+			if (this.zoomOut) {
+				this.realHeight = this.height
+			}
+			else {
+				this.realHeight = ""
+			}
+		}
+	}
 }
 </script>
 
 <style scoped>
+.img.zoom-out {
+	cursor: zoom-in;
+	width: 100%;
+	background-color: transparent;
+}
+
+.img.zoom-in {
+	position: fixed;
+	top: 0;
+	left: 0;
+	background-color: white;
+	z-index: 10000;
+	cursor: zoom-out;
+}
 </style>
